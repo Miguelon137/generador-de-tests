d96df58016665fc7b471445cd5b095468b3275ed
Miguelon137 <55759479+Miguelon137@users.noreply.github.com>
2020-03-03 19:55:41 +0100

    Add files via upload

---
+++ prueba.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<title>Encuesta</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body bgcolor="#00ff59">
+<p><font size="2" face="Arial, Helvetica, sans-serif"><strong><font color="#FFFFFF">Cual te parece mas correcta?
+ </font></strong></font></p>
+<form name="form1" method="post" action=".php">
+  <p> <font color="#FFFFFF" size="3" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="a">
+    <font color="#000000"><b>Aqui van la primera opción </font></font></p>
+  <p> <font color="#FFFFFF" size="3" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="b">
+    <font color="#000000">Aqui van la segunda opción </font></font></p>
+  <p> <font color="#FFFFFF">
+  <p> <font color="#FFFFFF" size="3" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="c">
+    <font color="#000000">Aqui van la tercera opción </font></font></p>
+  <p> <font color="#FFFFFF" size="3" face="Arial, Helvetica, sans-serif"> 
+    <input type="radio" name="op" value="d">
+    <font color="#000000">Aqui van la cuarta opción</b> </font></font></p>
+  <p> <font color="#FFFFFF">
+    <input type="submit" name="Submit" value="Enviar">
+    </font></p>
+</form>
+<p>&nbsp;</p>
+</body>
+</html>
+
+</html>
