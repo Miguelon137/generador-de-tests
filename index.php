c31b8fd1aa1d610e9dd3829e0577159d4722b3a9
jtaltavull <62353373+jtaltavull@users.noreply.github.com>
2020-03-25 19:31:41 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <Title>Index test</Title>
+    <style>
+    </style>
+</head>
+<body>
+   <h1>Bienvenido al generador de testos</h1>
+<?php
+   $db = new Sqlite3('jornadas.db');
+   $db->exec("CREATE TABLE IF NOT EXISTS jornadas(id INTEGER PRIMARY KEY, opcio1 text not null, opcio2 text not null, opcio3 text not null, resultado varchar not null)");
+      if (isset($_POST['submit'])){
+          /*aqui guardamos*/
+          $resultado = $_POST['resultado'];
+          $opcio1 = $_POST['opcio1'];
+          $opcio2 = $_POST['opcio2'];
+          $opcio3 = $_POST['opcio3'];
+          $stm = $db-> prepare("INSERT INTO jornadas(jornada, equipo1, equipo2, resultado) VALUES (?, ?, ?, ?)")
+          $stm->bindParam(1, $opcio1);
+          $stm->bindParam(2, $opcio2);
+          $stm->bindParam(3, $opcio3);
+          $stm->bindParam(4, $resultado);
+          $stm->execute();
+       
+?>
+<form action="index.php" method="post">
+    <p><input type="submit" name= "submit" value="Comienzo"/></p>    
+</body>
+</html>
