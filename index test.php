0f964a856483ae7e05286bcb9318228d0580ebf0
Miguelon137 <55759479+Miguelon137@users.noreply.github.com>
2020-03-25 19:17:53 +0100

    Add files via upload

---
+++ index test.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <Title>Index test</Title>
+    <style>
+    </style>
+</head>
+<body>
+   <h1>Bienvenido al generador de testos</h1>
+<?php
+    
+?>
+<form action=".php" method="post">
+    <p><input type="submit" name= "submit" value="Comienzo"/></p>    
+</body>
+</html>
