1ccae22733696699cad947b4f5b6c6cc53a46172
Miguelon137 <55759479+Miguelon137@users.noreply.github.com>
2020-03-25 19:52:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,12 +10,12 @@
    $db = new Sqlite3('test.db');
    $db->exec("CREATE TABLE IF NOT EXISTS test(id INTEGER PRIMARY KEY, opcio1 text not null, opcio2 text not null, opcio3 text not null, resultado varchar not null)");
       if (isset($_POST['submit'])){
-         
+          /*aqui guardamos*/
           $resultado = $_POST['resultado'];
           $opcio1 = $_POST['opcio1'];
           $opcio2 = $_POST['opcio2'];
-          $opcio3 = $_POST['opcio3'];
-          $stm = $db-> prepare("INSERT INTO test(opcio1, opcio2, opcio3, resultado) VALUES (?, ?, ?, ?)")
+          $opcio3 = $_POST['opcio3'];}
+          $stm = $db-> prepare("INSERT INTO test(test, opcio1, opcio2, opcio3, resultado) VALUES (?, ?, ?, ?)");
           $stm->bindParam(1, $opcio1);
           $stm->bindParam(2, $opcio2);
           $stm->bindParam(3, $opcio3);
@@ -23,8 +23,8 @@
           $stm->execute();
        
 ?>
-<form action="index.php" method="post">
-    <p><input type="submit" name= "submit" value="Comienzo"/></p>    
- </form>
+<form action="cuestionario.php" method="post">
+    <p><input type="submit" name= "submit" value="Comienzo"/></p>
+</form>    
 </body>
 </html>
