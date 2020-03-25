5826b02ea9ed23fc6e67eec55881c1d14b49aeac
jtaltavull <62353373+jtaltavull@users.noreply.github.com>
2020-03-25 19:38:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,15 +7,15 @@
 <body>
    <h1>Bienvenido al generador de testos</h1>
 <?php
-   $db = new Sqlite3('jornadas.db');
-   $db->exec("CREATE TABLE IF NOT EXISTS jornadas(id INTEGER PRIMARY KEY, opcio1 text not null, opcio2 text not null, opcio3 text not null, resultado varchar not null)");
+   $db = new Sqlite3('test.db');
+   $db->exec("CREATE TABLE IF NOT EXISTS test(id INTEGER PRIMARY KEY, opcio1 text not null, opcio2 text not null, opcio3 text not null, resultado varchar not null)");
       if (isset($_POST['submit'])){
-          /*aqui guardamos*/
+         
           $resultado = $_POST['resultado'];
           $opcio1 = $_POST['opcio1'];
           $opcio2 = $_POST['opcio2'];
           $opcio3 = $_POST['opcio3'];
-          $stm = $db-> prepare("INSERT INTO jornadas(jornada, equipo1, equipo2, resultado) VALUES (?, ?, ?, ?)")
+          $stm = $db-> prepare("INSERT INTO jornadas(jornada, opcio1, opcio2, opcio3, resultado) VALUES (?, ?, ?, ?)")
           $stm->bindParam(1, $opcio1);
           $stm->bindParam(2, $opcio2);
           $stm->bindParam(3, $opcio3);
