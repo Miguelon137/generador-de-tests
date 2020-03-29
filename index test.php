b50708cf1ed80d9f8a5424f8e857187c052bd461
Miguelon137 <55759479+Miguelon137@users.noreply.github.com>
2020-03-29 18:47:56 +0200

    Update index test.php

--- index test.php
+++ index test.php
@@ -6,10 +6,13 @@
 </head>
 <body>
    <h1>Bienvenido al generador de testos</h1>
+   <br><br><br>
+   <form action="cuestionario.php" method="POST">
+   <p>Escribe tu nombre:<br> <input type="text" name="nombre" /></p>
 <?php
     
 ?>
-<form action=".php" method="post">
+<form method="post">
     <p><input type="submit" name= "submit" value="Comienzo"/></p>    
 </body>
 </html>
